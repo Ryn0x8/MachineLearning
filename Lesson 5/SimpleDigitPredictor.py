0d825bc8de1c87ba47e6864b1510abc081faad10
InfernoblzDEV <saralthapaliya1@gmail.com>
2025-11-18 20:27:04 +0545

    Fixed

--- Lesson 5/SimpleDigitPredictor.py
+++ Lesson 5/SimpleDigitPredictor.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
-from tensorflow.keras import layers, models
+from tensorflow import keras
+from keras import layers, models
 import matplotlib.pyplot as plt
 
-(x_train, y_train), (x_test, y_test) =  tf.keras.datasets.mnists.load_data()
+(x_train, y_train), (x_test, y_test) =  tf.keras.datasets.mnist.load_data()
 
 x_train, x_test = x_train/255.0, x_test/255.0
 
