2c5ea49dfba4f02aae2be19f354d9b5c1522a50d
InfernoblzDEV <saralthapaliya1@gmail.com>
2025-11-25 18:37:32 +0545

    Done ACP

--- Lesson 5/SimpleDigitPredictor.py
+++ Lesson 5/SimpleDigitPredictor.py
@@ -7,6 +7,18 @@ import matplotlib.pyplot as plt
 
 x_train, x_test = x_train/255.0, x_test/255.0
 
+x_train = x_train[..., tf.newaxis]
+x_test = x_test[..., tf.newaxis]
+
+data_augemtation = keras.Sequential(
+    [
+        layers.RandomRotation(0.1),
+        layers.RandomZoom(0.1),
+        layers.RandomTranslation(0.1, 0.1),
+    ]
+)
+
+
 model = models.Sequential([
     layers.Flatten(input_shape = (28,28)),
     layers.Dense(128, activation = "relu"),
@@ -14,18 +26,32 @@ model = models.Sequential([
 ])
 
 model.compile(
-            optimizer = "adam", 
+            optimizer = "rmsprop", 
             loss = "sparse_categorical_crossentropy", 
             metrics = ["accuracy"]
             )
 
-model.fit(x_train, y_train, epochs = 5)
+history = model.fit(x_train, y_train, epochs = 5, validation_split=0.1)
 
 test_loss, test_acc = model.evaluate(x_test, y_test)
 print('\nTest accuracy:', test_acc)
 
 predictions = model.predict(x_test)
 
-plt.imshow(x_test[0], cmap=plt.cm.binary)
-plt.title(f"Predicted Label: {predictions[0].argmax()}")
+plt.figure(figsize=(4,4))
+plt.imshow(x_test[0].reshape(28,28), cmap="gray")
+plt.title(f"Predicted: {predictions[0].argmax()}")
+plt.axis("off")
+plt.show()
+
+plt.plot(history.history['accuracy'], label="Train Accuracy")
+plt.plot(history.history['val_accuracy'], label="Validation Accuracy")
+plt.legend()
+plt.title("Accuracy Curve")
+plt.show()
+
+plt.plot(history.history['loss'], label="Train Loss")
+plt.plot(history.history['val_loss'], label="Validation Loss")
+plt.legend()
+plt.title("Loss Curve")
 plt.show()
